1c9a9867c25dfcdb5f9b121412296d32cbbb4998
deecodecracker <deepak.j.a.30@gmail.com>
2021-11-14 00:51:56 +0530

    Create StartingProj.java
    First commit

---
+++ src/javadesignpatterns1/StartingProj.java
@@ -0,0 +1,13 @@
+package javadesignpatterns1;
+
+public class StartingProj {
+
+	public static void main(String[] args) {
+		m1();
+	}
+
+	private static void m1() {
+		System.out.println("This is m1 being called..");
+	}
+
+}
