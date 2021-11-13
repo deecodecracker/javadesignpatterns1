12525f6e0a76cca3b35add724a77eacd97bba145
deecodecracker <deepak.j.a.30@gmail.com>
2021-11-14 00:55:05 +0530

    Update StartingProj.java
    changing method name

--- src/javadesignpatterns1/StartingProj.java
+++ src/javadesignpatterns1/StartingProj.java
@@ -3,10 +3,10 @@ package javadesignpatterns1;
 public class StartingProj {
 
 	public static void main(String[] args) {
-		m1();
+		myHello();
 	}
 
-	private static void m1() {
+	private static void myHello() {
 		System.out.println("This is m1 being called..");
 	}
 
